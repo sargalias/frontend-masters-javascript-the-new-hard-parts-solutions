d4fc22ae890bdb65dc09fa33e08647261c2d090d
Spyros Argalias <sargalias@gmail.com>
2019-08-02 22:07:39 +0100

    feat(promises): Complete sayHello function

--- promises.js
+++ promises.js
@@ -1,11 +1,11 @@
 // Challenge 1
 
 function sayHello() {
-
+  setTimeout(() => console.log('Hello'), 1000);
 }
 
 // Uncomment the line below when ready
-// sayHello(); // should log "Hello" after 1000ms
+sayHello(); // should log "Hello" after 1000ms
 
 
 // Challenge 2
